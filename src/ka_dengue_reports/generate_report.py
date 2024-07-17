65ab18d5b0085e9d71271aaa4cc6e1c7f84c4ff5
anujavenkatachalam04 <anuja.venkatachalam@ifmr.ac.in>
2024-07-17 15:33:28 +0530

    major: changes to district assignment for BBMP and table format

--- src/ka_dengue_reports/generate_report.py
+++ src/ka_dengue_reports/generate_report.py
@@ -5,6 +5,7 @@ from dataio.download import download_dataset_v2
 
 from ka_dengue_reports import get_regionIDs
 
+
 analysis_window = 7
 report_file_name = "reports/KA Den Report 17 July.md"
 
@@ -15,6 +16,11 @@ ka_districts = regionIDs_df[regionIDs_df["parentID"]=="state_29"].reset_index(dr
 download_dataset_v2(dsid="EP0005DS0014", contains_any = "ihip")
 
 df = pd.read_csv("data/EP0005DS0014-KA_Dengue_LL/ihip/ka-line-list-ihip.csv")
+
+# changing reporting for BBMP 
+
+df.loc[df["location.admin3.ID"]=="ulb_276600", "location.admin2.ID"]=df["location.admin3.ID"]
+df.loc[df["location.admin3.ID"]=="ulb_276600", "location.admin2.name"]=df["location.admin3.name"]
 
 df = df[['location.admin2.ID',
        'location.admin2.name', 'location.admin3.ID', 'location.admin3.name',
@@ -40,7 +46,6 @@ hotspots['whether_hotspot'] = hotspots['number_of_cases'].apply(lambda x: x >= 2
 # District Table
 
 district_table = df.groupby(by="location.admin2.ID")["event.test"].sum().reset_index().rename(columns={"event.test":"total_cases_by_district"})  # noqa: E501
-district_table = district_table.merge(hotspots.groupby(by=["location.admin2.ID", "location.admin2.name"])["location.admin3.ID"].count().reset_index().rename(columns={"location.admin3.ID":"cases_with_info"}), on="location.admin2.ID", how="left")  # noqa: E501
 district_table = district_table.merge(hotspots.groupby(by="location.admin2.ID")["whether_hotspot"].sum().reset_index().rename(columns={"whether_hotspot":"number_of_hotspots"}), on="location.admin2.ID", how="left") # noqa: E501
 district_table = district_table.merge(ka_districts, left_on = "location.admin2.ID",
                                       right_on = "regionID", how = "right").drop(columns = ["location.admin2.ID",
@@ -49,16 +54,15 @@ district_table["district_code"] = district_table["regionID"].str.split("_").str.
 
 district_table = district_table.drop(columns = ["regionID"])
 district_table = district_table[["district_code", "location.admin2.name", "total_cases_by_district",
-                                 "cases_with_info","number_of_hotspots"]]
+                                 "number_of_hotspots"]]
 district_table = district_table.rename(columns = {"location.admin2.name":"district_name"})
 
 district_table.loc[len(district_table.index)] = [pd.NA, "Total", district_table["total_cases_by_district"].sum(),
-                                                 district_table["cases_with_info"].sum(), district_table["number_of_hotspots"].sum()]
+                                                    district_table["number_of_hotspots"].sum()]
 
 district_table = district_table.rename(columns = {"district_code":"District Code",
                                                   "district_name":"District Name", 
-                                                  "total_cases_by_district":"Total Cases",
-                                                  "cases_with_info":"Cases with Village/Ward Info",
+                                                  "total_cases_by_district":"Reported Cases",
                                                   "number_of_hotspots":"Number of Hotspots"})
 district_dict = district_table.fillna("").to_dict(orient='records')
 
@@ -73,10 +77,8 @@ header = f"""### Karnataka Dengue Report, dtd. {datetime.datetime.today().strfti
 #### Summary
 * **Report Period**: {min_date.strftime('%B %d %Y')} - {max_result_date.strftime('%B %d %Y')}
 * **Analysis Window**: {analysis_window} days
-* **Total Cases**: {len(df)} cases were reported in this time period.
+* **Reported Cases**: {len(df)} cases were reported in this time period.
 * **Hotspots**: {len(hotspots[hotspots["whether_hotspot"]])} villages/wards were identified as hotspots (with 2 or more cases).
-* **Location Information Availability**: Out of {len(df)} cases, {cases_with_village_ward_info} cases have village/ward level information and {cases_with_subdistrict_ulb_info} cases have subdistrict/ULB level information.
-
 """
 
 with open(report_file_name, "w") as f:
@@ -84,3 +86,7 @@ with open(report_file_name, "w") as f:
 
 datadict_df.to_markdown(buf=report_file_name, index=False, mode = "ab")
 
+footer = f"""
+##### Footnote
+* **Location Information Availability**: Out of {len(df)} cases, {cases_with_village_ward_info} cases have village/ward level information and {cases_with_subdistrict_ulb_info} cases have subdistrict/ULB level information.
+"""
